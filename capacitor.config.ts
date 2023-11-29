3c76e3b8014b1219528b07cc4768acb14ca8f1c3
Ryan Bosher <ryan@bosher.co.nz>
2023-11-29 23:57:03 +1300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cordova-iab-6-example',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
